7b3370a473f56ccbe568cd9137f9a6ffc1e8333d
ImmoSS <83536444+ImmoSS@users.noreply.github.com>
2024-03-30 00:36:45 +0300

    1.3

--- ui_check.bat
+++ ui_check.bat
@@ -1,7 +1,7 @@
 @ECHO off
 pushd "%~dp0"
 cd ..
-set patchfolder=KEK Mod v1.2
+set patchfolder=KEK Mod v1.3
 ECHO Y | del "%cd%\%patchfolder%\UI\"
 REM -------------------------------------------------
 ECHO F | xcopy /s /y "%cd%\%patchfolder%\tmp\ui\CultureOverview.lua" "%cd%\%patchfolder%\UI\CultureOverview.lua"
